95cef70034195e5aa5c9992bec588634562295da
Jose Carlos Huerta Garcia <jcarloshg@MacBook-Air-de-Jose.local>
2024-06-27 17:45:19 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$pepe = "Pepe";
+?>
+
+<h1>
+    <?= $pepe; ?>
+</h1>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        display: grid;
+        place-content: center;
+    }
+</style>
